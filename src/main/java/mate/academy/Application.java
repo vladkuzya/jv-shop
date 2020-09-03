7f7ba7a0db2dd811373a6e247e37ebc1f30f4bff
Vlad Kuzmin <vladkuzmin22@gmail.com>
2020-09-03 13:06:56 +0300

    edit price for all products, fixed test of update product

--- src/main/java/mate/academy/Application.java
+++ src/main/java/mate/academy/Application.java
@@ -5,7 +5,6 @@ import mate.academy.model.Product;
 import mate.academy.service.ProductService;
 
 public class Application {
-    private static final long ID_UPDATE = 3L;
     private static final long ID_GET = 2L;
     private static final long ID_DELETE = 1L;
 
@@ -13,17 +12,20 @@ public class Application {
 
     public static void main(String[] args) {
         ProductService productService = (ProductService) injector.getInstance(ProductService.class);
-        productService.create(new Product("iphoneX", 20000));
-        productService.create(new Product("iphone 11", 25000));
-        productService.create(new Product("iphone 7", 10000));
-        Product iphone8 = new Product("iphone 8", 10000);
-        iphone8.setId(ID_UPDATE);
+        productService.create(new Product("iphoneX", 700));
+        productService.create(new Product("iphone 11", 950));
+        productService.create(new Product("iphone 7", 400));
+        Product iphone8 = new Product("iphone 8", 500);
+        productService.create(iphone8);
+        System.out.println(productService.getAll());
 
+        iphone8.setPrice(400);
         productService.update(iphone8);
+        System.out.println(productService.getAll());
+
         System.out.println(productService.get(ID_GET));
 
         productService.delete(ID_DELETE);
         System.out.println(productService.getAll());
-
     }
 }
